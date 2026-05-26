83fb199e9b7dff8c99a30dd38bc8aa671041164d
Dev Melo <omelodev@gmail.com>
2026-05-26 18:10:36 -0300

    operadores de comparação

---
+++ operador_comparacao.js
@@ -0,0 +1,21 @@
+/*
+console.log(50 > 3);
+console.log(50 < 3);
+
+console.log(50 <= 3);
+console.log(50 <= 300);
+console.log(50 <= 50);
+
+console.log(50 >= 3);
+console.log(50 >= 300);
+console.log(50 >= 50);
+*/
+
+// estritamente
+
+console.log("2" == 2); // testa o valor
+console.log("2" === 2); // testa o valor e o tipo
+
+
+console.log("2" !== 2); // testa o valor e o tipo
+console.log("2" != 2); // testa o valor 
