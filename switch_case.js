cb5fa75e7d5ae43df30e37e915c96b72d6054817
Dev Melo <omelodev@gmail.com>
2026-05-26 20:03:38 -0300

    switch

---
+++ switch_case.js
@@ -0,0 +1,24 @@
+const animal = "cobra";
+switch(animal){
+    case "gato":{
+        console.log("Miau");
+        break;
+    }
+    case "cachorro":{
+        console.log("Au Au");
+        break;
+    }
+    case "pato":{
+        console.log("Quack");
+        break;
+    }
+    case "cobra":{
+        console.log("Shhhhh");
+        break;
+    }
+    case "macaco":{
+        console.log("Uuhuu Aahaa");
+        break;
+    }
+    default:{console.log("não é animal")}
+}
